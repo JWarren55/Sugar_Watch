fb9aff190b8b2d379173dea233d6a430e9066807
JWarren55 <jonwwarren55@gmail.com>
2026-08-27 07:41:47 -0500

    -Add google auth to main -Add user info to index.html w/ profile picture

--- main.py
+++ main.py
@@ -1,19 +1,76 @@
+import os
+
+from authlib.integrations.starlette_client import OAuth
+from dotenv import load_dotenv
 from fastapi import FastAPI, Request
-from fastapi.templating import Jinja2Templates
+from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from starlette.middleware.sessions import SessionMiddleware
+
+
+load_dotenv()
 
 app = FastAPI()
+
+app.add_middleware(
+    SessionMiddleware,
+    secret_key=os.getenv("SECRET_KEY")
+)
 
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
+oauth = OAuth()
+
+oauth.register(
+    name="google",
+    client_id=os.getenv("GOOGLE_CLIENT_ID"),
+    client_secret=os.getenv("GOOGLE_CLIENT_SECRET"),
+    server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
+    client_kwargs={
+        "scope": "openid email profile"
+    }
+)
+
+
 @app.get("/")
 def home(request: Request):
+    user = request.session.get("user")
+
     return templates.TemplateResponse(
         request=request,
         name="index.html",
         context={
-            "title": "Sugar Watch"
+            "title": "Sugar Watch",
+            "user": user
         }
     )
+
+
+@app.get("/login")
+async def login(request: Request):
+    redirect_uri = request.url_for("google_callback")
+    return await oauth.google.authorize_redirect(request, redirect_uri)
+
+
+@app.get("/auth/google/callback")
+async def google_callback(request: Request):
+    token = await oauth.google.authorize_access_token(request)
+
+    user = token.get("userinfo")
+
+    request.session["user"] = {
+        "name": user.get("name"),
+        "email": user.get("email"),
+        "picture": user.get("picture")
+    }
+
+    return RedirectResponse(url="/")
+
+
+@app.get("/logout")
+def logout(request: Request):
+    request.session.clear()
+    return RedirectResponse(url="/")
